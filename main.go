099168e0b545ee75c4959b528935160bb7241f92
Florian Rand <florian.rand@gmail.com>
2021-11-05 12:37:52 +0100

    Move outside main

--- main.go
+++ main.go
@@ -2,41 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// const energyFullDesign = "/sys/class/power_supply/BAT0/energy_full_design"
-const energyFull = "/sys/class/power_supply/BAT0/energy_full"
-const energyNow = "/sys/class/power_supply/BAT0/energy_now"
-
-// const powerNow = "/sys/class/power_supply/BAT0/power_now"
-// const batteryStatus = "/sys/class/power_supply/BAT0/status"
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func parseBatteryValue(d []byte) int {
-	v, err := strconv.Atoi(strings.Trim(string(d), "\n"))
-	check(err)
-
-	return v / 1000
-}
-
-func calculateBatteryPercentage(current, full int) int {
-	return current * 100 / full
-}
-
 func main() {
+
 	homeDir, err := os.UserHomeDir()
 	check(err)
 
@@ -57,15 +30,6 @@ func main() {
 	check(err)
 	defer batteryDB.Close()
 	createTable(batteryDB)
-
-	// Read the current battery value
-	current, err := ioutil.ReadFile(energyNow)
-	check(err)
-	energyFull, err := ioutil.ReadFile(energyFull)
-	check(err)
-	percentage := calculateBatteryPercentage(parseBatteryValue(current), parseBatteryValue(energyFull))
-	fmt.Printf("%d%%\n", percentage)
-
 }
 
 func createTable(batteryDB *sql.DB) {
