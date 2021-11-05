2845891aa87b1b15e9fc5a8f0f33f594932b534a
Florian Rand <florian.rand@gmail.com>
2021-11-05 15:32:55 +0100

    Create trim value func

--- util/battery_utility.go
+++ util/battery_utility.go
@@ -13,8 +13,12 @@ func ParseMilliWats(m int) float32 {
 	return float32(m) / 1000
 }
 
+func TrimValue(d []byte) string {
+	return strings.Trim(string(d), "\n")
+}
+
 func ParseBatteryValue(d []byte) int {
-	v, err := strconv.Atoi(strings.Trim(string(d), "\n"))
+	v, err := strconv.Atoi(TrimValue(d))
 	Check(err)
 
 	return v / 1000
