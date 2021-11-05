7ae63046a31f4861db9dc7d2ef53d3856bd9d437
Florian Rand <florian.rand@gmail.com>
2021-11-05 13:05:15 +0100

    Add parse milliwats

--- util/battery_utility.go
+++ util/battery_utility.go
@@ -9,6 +9,10 @@ func CalculateBatteryPercentage(current, full int) int {
 	return current * 100 / full
 }
 
+func ParseMilliWats(m int) float32 {
+	return float32(m) / 1000
+}
+
 func ParseBatteryValue(d []byte) int {
 	v, err := strconv.Atoi(strings.Trim(string(d), "\n"))
 	Check(err)
