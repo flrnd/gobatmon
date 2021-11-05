e3a8af662880f3373aa4fdb6d2f4105ffdb2d8d1
Florian Rand <florian.rand@gmail.com>
2021-11-05 13:04:54 +0100

    Print basic stats

--- cmd/stats.go
+++ cmd/stats.go
@@ -5,9 +5,11 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/flrnd/gobatmon/util"
 	"github.com/spf13/cobra"
 )
 
+const cycleCountPath = "/sys/class/power_supply/BAT0/cycle_count"
 const energyFullDesignPath = "/sys/class/power_supply/BAT0/energy_full_design"
 const energyFullPath = "/sys/class/power_supply/BAT0/energy_full"
 const energyNowPath = "/sys/class/power_supply/BAT0/energy_now"
@@ -19,13 +21,30 @@ var statsCmd = &cobra.Command{
 	Long: `Print battery stats like full charge
 								full power, full fuck you...`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// Read design full charge
+		fullDesign, err := ioutil.ReadFile(energyFullDesignPath)
+		util.Check(err)
+		// Read full charge
+		energyFull, err := ioutil.ReadFile(energyFullPath)
+		util.Check(err)
 		// Read the current battery value
 		current, err := ioutil.ReadFile(energyNowPath)
-		Check(err)
-		energyFull, err := ioutil.ReadFile(energyFullPath)
-		Check(err)
-		percentage := CalculateBatteryPercentage(ParseBatteryValue(current), ParseBatteryValue(energyFull))
-		fmt.Printf("%d%%\n", percentage)
+		util.Check(err)
+		percentage := util.CalculateBatteryPercentage(util.ParseBatteryValue(current), util.ParseBatteryValue(energyFull))
+		// read power
+		power, err := ioutil.ReadFile(powerNowPath)
+		util.Check(err)
+
+		// read cycles
+		cycles, err := ioutil.ReadFile(cycleCountPath)
+		util.Check(err)
+
+		// print the battery stats
+		fmt.Printf("Full design capacity: %dmWh\n", util.ParseBatteryValue(fullDesign))
+		fmt.Printf("Full charge capacity: %dmWh\n", util.ParseBatteryValue(energyFull))
+		fmt.Printf("Current charge at: %d%%\n", percentage)
+		fmt.Printf("Cycle count: %s", string(cycles))
+		fmt.Printf("Current power discharge at: %.2fW\n", util.ParseMilliWats(util.ParseBatteryValue(power)))
 	},
 }
 
