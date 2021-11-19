6e58f7253809789b6b61faa4acdc341b27e47f39
Florian Rand <florian.rand@gmail.com>
2021-11-19 20:16:47 +0100

    Add last command

---
+++ cmd/last.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/flrnd/gobatmon/db"
+	"github.com/flrnd/gobatmon/util"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(lastCmd)
+}
+
+var lastCmd = &cobra.Command{
+	Use:   "last",
+	Short: "print discharge % since the last recorded timestamp",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		currentCharge := util.Stats().CurrentCharge
+		currentTime := time.Now()
+		_, charge, timestamp := db.Last()
+		fmt.Printf("%d%% discharge in %v", util.CalculateDischarge(currentCharge, charge), currentTime.Sub(timestamp))
+	},
+}
