758c73b7dc7fe43221390d2b33c1070f59bf579f
Florian Rand <florian.rand@gmail.com>
2021-11-19 17:08:24 +0100

    Fix database panic

--- db/database.go
+++ db/database.go
@@ -2,8 +2,9 @@ package db
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/flrnd/gobatmon/util"
 )
@@ -22,29 +23,42 @@ func Init() {
 
 	if _, err := os.Stat(databasePath); os.IsNotExist(err) {
 		os.MkdirAll(homeDir+"/.config/batmon", 0755)
-		log.Println("Creating the database")
-		if file, err := os.Create(databasePath); err != nil {
-			util.Check(err)
-		} else {
-			file.Close()
-			log.Println("Database created")
-		}
+		// log.Println("Creating the database")
+		file, err := os.Create(databasePath)
+		util.Check(err)
+
+		file.Close()
+		// log.Println("Database created")
 	}
 
 	// Open the database
-	if batteryDB, err := sql.Open("sqlite3", databasePath); err != nil {
-		util.Check(err)
-	} else {
-		batteryDB.Close()
-		defer batteryDB.Close()
-		createTable(batteryDB)
-	}
+	batteryDB, err := sql.Open("sqlite3", databasePath)
+	util.Check(err)
+	defer batteryDB.Close()
+	createTable(batteryDB)
+
+}
+
+func Insert(charge string, timestamp time.Time) {
+	db, err := sql.Open("sqlite3", Path())
+	util.Check(err)
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO battery_charge(charge, timestamp) values(?,?)")
+	util.Check(err)
+
+	res, err := stmt.Exec(charge, timestamp)
+	util.Check(err)
+
+	id, err := res.LastInsertId()
+
+	fmt.Printf("created timestamp: %d\n %s - %s\n", id, timestamp.UTC(), charge)
 }
 
 func createTable(batteryDB *sql.DB) {
 	createBatteryTable := `CREATE TABLE IF NOT EXISTS "battery_charge" (
 		"id"	INTEGER NOT NULL UNIQUE,
-		"charge"	INTEGER NOT NULL,
+		"charge"	string NOT NULL,
 		"timestamp"	INTEGER NOT NULL,
 		PRIMARY KEY("id" AUTOINCREMENT));`
 
