d1c3f8da85802f938b26615971cec05590ceaba1
Florian Rand <florian.rand@gmail.com>
2021-12-20 15:39:56 +0100

    update import

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"github.com/flrnd/gobatmon/cmd"
-	"github.com/flrnd/gobatmon/db"
+	database "github.com/flrnd/gobatmon/db"
 )
 
 func main() {
-	db.Init()
+	database.Init()
 	cmd.Execute()
 }
