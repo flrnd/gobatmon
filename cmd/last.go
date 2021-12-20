dce8aa0d1de51d4a4074feb08cf4718c546bd64c
Florian Rand <florian.rand@gmail.com>
2021-12-20 16:32:23 +0100

    Add long help strings

--- cmd/last.go
+++ cmd/last.go
@@ -15,8 +15,8 @@ func init() {
 
 var lastCmd = &cobra.Command{
 	Use:   "last",
-	Short: "print discharge % since the last recorded timestamp",
-	Long:  "",
+	Short: "print discharge % since the last recorded timestamp. [help last] for more information",
+	Long:  "Print the discharge percentage since last recorded timestamp. Accepts save flag to create an entry on the database.",
 	Run: func(cmd *cobra.Command, args []string) {
 		_, charge, timestamp := db.Last()
 
