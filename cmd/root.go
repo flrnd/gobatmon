675e4e59ab5c2c87e38f2c74b52c464aa202ba43
Florian Rand <florian.rand@gmail.com>
2022-04-06 16:44:12 +0200

    Remove fmt.Fprintln to avoid duplicate error output
    See (https://github.com/spf13/cobra/issues/1415)

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,14 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{Use: "gobatmon"}
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	err := rootCmd.Execute()
+	if err != nil {
+		return
 	}
 }
